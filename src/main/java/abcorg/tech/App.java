c622745a79a36fb792d2290d96357b97459b41c1
gaurarvind <rishiarvind999@gmail.com>
2022-10-19 13:43:54 +0530

    test

---
+++ src/main/java/abcorg/tech/App.java
@@ -0,0 +1,13 @@
+package abcorg.tech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
